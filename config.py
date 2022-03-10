7467a152644ca1a4043354f87ac288b92a6ba466
rmasiakiewicz <rafal.masiakiewicz@dealavo.com>
2022-03-10 03:44:09 +0100

    delete db urls

--- config.py
+++ config.py
@@ -7,10 +7,7 @@ class Config:
 
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://sdawxqghbuigjs:9a742e28c8afeb5417506f0c1599f3c93444b1748d7062e5320228"
-        "4a43ee7e28@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d9i6b43829kk3l"
-    )
+    SQLALCHEMY_DATABASE_URI = ""
     TESTING = True
     TRAP_BAD_REQUEST_ERRORS = True
     WTF_CSRF_ENABLED = False
